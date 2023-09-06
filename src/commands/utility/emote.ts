981b633a9c955ff3c38315b73b7623ffc2057872
AkiraDev <akira.devjs@gmail.com>
2023-09-05 18:04:39 -0700

    implement emoji steal

--- src/commands/utility/emote.ts
+++ src/commands/utility/emote.ts
@@ -5,13 +5,12 @@ export const emote: Command = {
   description: "Currently broken, don't use",
   options: opts,
   execute: function (interaction, args) {
-    // const emote = args.getString("emote", true);
-    // const name = args.getString("name", false) || emote.split(":")[1];
-    // const animated = emote.startsWith("<a:");
-    // const emojiId = emote.match(/([0-9]+)/) || "";
-    // console.log(emojiId);
-    // interaction.guild?.emojis.create({attachment: `https://cdn.discordapp.com/emojis/${emojiId}${animated ? '.gif' : '.png'}`, name});
-    // interaction.editReply({content: `Emote ${emote} has been stolen!`});
-    interaction.editReply({content: "This command is currently unimplemented."});
+    const emote = args.getString("emote", true);
+    const name = args.getString("name", false) || emote.split(":")[1];
+    const animated = emote.startsWith("<a:");
+    const emojiId = emote.match(/([0-9]+)/) || "";
+    interaction.guild?.emojis.create({attachment: `https://cdn.discordapp.com/emojis/${emojiId[0]}${animated ? '.gif' : '.png'}`, name});
+    interaction.editReply({content: `Emote ${emote} has been stolen!`});
+    // interaction.editReply({content: "This command is currently unimplemented."});
   }
 }
